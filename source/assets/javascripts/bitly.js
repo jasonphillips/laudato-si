0337ec770afa7f34ab15c58be42bf020b12fe0da
jphillips@ua.edu <jason@jasons-MacBook-Air.local>
2015-06-24 08:26:40 -0500

    fixed bitly, better request

--- source/assets/javascripts/bitly.js
+++ source/assets/javascripts/bitly.js
@@ -1,24 +1,13 @@
 (function() {
   window.bitly = function(long_url,callback){
-    bi = new URL("https://api-ssl.bitly.com/v3/shorten?");
-    var params = [
-        "login=o_7v5rsdsp7f",
-        "apiKey=R_b5e0101b56484f27959e0a3f4013f854",
-        "longUrl="+ encodeURIComponent(long_url)
-    ]
-    bi.search = "?"+params.join('&')
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange=function () {
-       if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-         var res = JSON.parse(xhr.responseText);
-         callback(res["data"]["url"])
-        } else {
-          alert('There was a problem with the request.');
-        }
-      }
+    var url = 'https://api-ssl.bitly.com/v3/shorten';
+    var params = {
+        "login":"o_7v5rsdsp7f",
+        "apiKey":"R_b5e0101b56484f27959e0a3f4013f854",
+        "longUrl":long_url
     }
-    xhr.open("GET",bi.toString());
-    xhr.send(null)
+    $.get(url,params,function(response) {
+      callback(response.data.url);
+    })
   };
 })()
